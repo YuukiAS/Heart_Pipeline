83431f2c7381f147053e6dabc7c625071bebf48d
YuukiAS <yuukias@unc.edu>
2024-08-23 15:34:45 -0400

    Validate short strain features

--- src/feature_extraction/short_axis/eval_strain_sax.py
+++ src/feature_extraction/short_axis/eval_strain_sax.py
@@ -13,22 +13,30 @@
 # limitations under the License.
 # ============================================================================
 import os
+import pandas as pd
+import numpy as np
+import math
+import matplotlib.pyplot as plt
+import nibabel as nib
 import argparse
 import shutil
-import pandas as pd
+import pickle
 from tqdm import tqdm
 from rpy2.robjects.packages import importr
 from rpy2.robjects.vectors import FloatVector
 
 import sys
 
+sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
+from ecg.ecg_processor import ECG_Processor
+
 sys.path.append(os.path.join(os.path.dirname(__file__), "../../.."))
 import config
 
 from utils.log_utils import setup_logging
-
 from utils.quality_control_utils import sa_pass_quality_control
-from utils.cardiac_utils import cine_2d_sa_motion_and_strain_analysis
+from utils.analyze_utils import plot_time_series_double_x, plot_time_series_double_x_y, analyze_time_series_derivative
+from utils.cardiac_utils import cine_2d_sa_motion_and_strain_analysis, evaluate_strain_by_length_sa
 
 logger = setup_logging("eval_strain_sax")
 
@@ -51,6 +59,12 @@ if __name__ == "__main__":
         seg_sa_name = os.path.join(sub_dir, "seg_sa.nii.gz")
         seg_sa_ED_name = os.path.join(sub_dir, "seg_sa_ED.nii.gz")
 
+        nim = nib.load(seg_sa_name)
+        T = nim.header["dim"][4]
+        temporal_resolution = nim.header["pixdim"][4]
+        time_grid_point = np.arange(T)
+        time_grid_real = time_grid_point * temporal_resolution  # unit: s
+
         if not os.path.exists(seg_sa_ED_name) or not os.path.exists(seg_sa_name):
             logger.error(f"Segmentation of short axis file for {subject} does not exist")
             continue
@@ -67,8 +81,6 @@ if __name__ == "__main__":
         # Note that we should not use export here, as it will exit after subprocess terminates
         os.environ["PATH"] = config.MIRTK_path + os.pathsep + os.environ["PATH"]
         os.environ["PATH"] = config.average_3d_ffd_path + os.pathsep + os.environ["PATH"]
-        # os.system(f"export PATH={config.MIRTK_path}:$PATH")
-        # os.system(f"export PATH={config.average_3d_ffd_path}:$PATH")
 
         # Define This file contains registration parameters for each resolution level
         par_config_name = os.path.join(config.par_config_dir, "ffd_cine_sa_2d_motion.cfg")
@@ -82,33 +94,314 @@ if __name__ == "__main__":
         if not os.path.exists(ft_dir):
             os.makedirs(ft_dir)
 
+        logger.info(f"{subject}: Perform motion tracking on short-axis images and calculate the strain.")
         # Perform motion tracking on short-axis images and calculate the strain
-        radial_strain, circum_strain = cine_2d_sa_motion_and_strain_analysis(sub_dir, par_config_name, temp_dir, ft_dir)
+        cine_2d_sa_motion_and_strain_analysis(sub_dir, par_config_name, temp_dir, ft_dir)
+        radial_strain, circum_strain = evaluate_strain_by_length_sa(
+            f"{ft_dir}/myo_contour_sa/myo_contour_fr", T, ft_dir
+        )
         logger.info(f"{subject}: Radial and circumferential strain calculated, remove intermediate files.")
 
         # Remove intermediate files
         shutil.rmtree(temp_dir)
 
+        # * We use the maximum absolute value: minimum for circumferential (negative) and maximum for radial (positive)
+
         for i in range(16):
-            feature_dict.update({
-                f"LV: Radial strain (AHA_{i + 1}) [%]": radial_strain[i, :].max(),
-                f"LV: Circumferential strain( AHA_{i + 1}) [%]": circum_strain[i, :].min(),
-            })
+            feature_dict.update(
+                {
+                    f"LV: Circumferential strain (AHA_{i + 1}) [%]": circum_strain[i, :].min(),
+                    f"LV: Radial strain (AHA_{i + 1}) [%]": radial_strain[i, :].max(),
+                }
+            )
 
-        feature_dict.update({
-            "LV: Radial strain (Global) [%]": radial_strain[16, :].max(),
-            "LV: Circumferential strain (Global) [%]": circum_strain[16, :].min(),
-        })
+        feature_dict.update(
+            {
+                "LV: Circumferential strain (Global) [%]": circum_strain[16, :].min(),
+                "LV: Radial strain (Global) [%]": radial_strain[16, :].max(),
+            }
+        )
 
-        # todo: Make a time series
+        # * Make a time series plot and store the time series of global strain
+        logger.info(f"{subject}: Plot and store time series of global strain.")
+
+        with open(f"{sub_dir}/timeseries/strain_sa.pkl", "wb") as time_series_file:
+            pickle.dump(
+                {
+                    "LV: Global Circumferential Strain (GCS) [%]": circum_strain[16, :],
+                    "LV: Global Radial Strain (GRS) [%]": radial_strain[16, :],
+                },
+                time_series_file,
+            )
+
+        fig, ax1, ax2 = plot_time_series_double_x(
+            time_grid_point,
+            time_grid_real * 1000,
+            circum_strain[16, :],
+            "Time [frame]",
+            "Time [ms]",
+            "Strain [%]",
+            lambda x: x * temporal_resolution,
+            lambda x: x / temporal_resolution,
+            title=f"Subject {subject}: Global Circumferential Strain (GCS) Time Series (Raw)",
+        )
+        fig.savefig(f"{sub_dir}/timeseries/gcs_raw.png")
+        plt.close(fig)
+
+        fig, ax1, ax2 = plot_time_series_double_x(
+            time_grid_point,
+            time_grid_real * 1000,
+            radial_strain[16, :],
+            "Time [frame]",
+            "Time [ms]",
+            "Strain [%]",
+            lambda x: x * temporal_resolution,
+            lambda x: x / temporal_resolution,
+            title=f"Subject {subject}: Global Radial Strain (GRS) Time Series (Raw)",
+        )
+        fig.savefig(f"{sub_dir}/timeseries/grs_raw.png")
+        plt.close(fig)
+
+        # Read in time points
+        with open(f"{sub_dir}/timeseries/ventricle.pkl", "rb") as time_series_file:
+            ventricle = pickle.load(time_series_file)
+
+        with open(f"{sub_dir}/timeseries/atrium.pkl", "rb") as time_series_file:
+            atrium = pickle.load(time_series_file)
+
+        T_ES = ventricle["LV: T_ES"]
+        T_1_3_DD = T_ES + math.ceil((50 - T_ES) / 3)
+        T_pre_a = None
+
+        try:
+            T_pre_a = atrium["LA: T_pre_a"]
+        except KeyError:
+            logger.warning(f"{subject}: No atrial contraction time information for strain calculation.")
 
         # * Feature 1: Strain rate
 
-        radial_strain_lowess = None
+        try:
+            T_GCSR_pos, T_GCSR_neg, GCSR_pos, GCSR_neg = analyze_time_series_derivative(
+                time_grid_real,
+                circum_strain[16, :] / 100,  # Since strain is in %
+                n_pos=2,
+                n_neg=1,
+                method="loess"  # for strain rate, we don't use the moving average method
+            )
 
-        # * We use the maximum absolute value: minimum for circumferential (negative) and maximum for radial (positive)
-        target_dir = config.features_visit2_dir if args.retest else config.features_visit1_dir
-        target_dir = os.path.join(target_dir, "strain")
-        os.makedirs(target_dir, exist_ok=True)
-        df.sort_index(axis=1, inplace=True)  # sort the columns according to alphabet orders
-        df.to_csv(os.path.join(target_dir, f"{args.file_name}.csv"), ignore_index=True)
+            GCSR_S = GCSR_neg[0]
+            GCSR_E = GCSR_pos[np.argmin(T_GCSR_pos)]
+            GCSR_A = GCSR_pos[np.argmax(T_GCSR_pos)]
+            if np.min(T_GCSR_pos) < T_ES:
+                raise ValueError("Time for peak early diastolic circumferential strain rate should be after ES.")
+            feature_dict.update(
+                {
+                    "LV: Circumferential Strain Rate (Peak-systolic) [1/s]": GCSR_S,
+                    "LV: Circumferential Strain Rate (Early-diastole) [1/s]": GCSR_E,
+                    "LV: Circumferential Strain Rate (Late-diastole) [1/s]": GCSR_A,
+                }
+            )
+            logger.info(f"{subject}: Global circumferential strain rate calculated.")
+        except ValueError as e:
+            logger.warning(f"{subject}: {e}  No global circumferential strain rate calculated.")
+
+        try:
+            T_GRSR_pos, T_GRSR_neg, GRSR_pos, GRSR_neg = analyze_time_series_derivative(
+                time_grid_real,
+                radial_strain[16, :] / 100,  # Since strain is in %
+                n_pos=1,
+                n_neg=2,
+                method="loess"
+            )
+            GRSR_S = GRSR_pos[0]
+            GRSR_E = GRSR_neg[np.argmin(T_GRSR_neg)]
+            GRSR_A = GRSR_neg[np.argmax(T_GRSR_neg)]
+            if np.min(T_GRSR_neg) < T_ES:
+                raise ValueError("Time for peak early diastolic radial strain rate should be after ES.")
+            feature_dict.update(
+                {
+                    "LV: Radial Strain Rate (Peak-systolic) [1/s]": GRSR_S,
+                    "LV: Radial Strain Rate (Early-diastole) [1/s]": GRSR_E,
+                    "LV: Radial Strain Rate (Late-diastole) [1/s]": GRSR_A,
+                }
+            )
+            logger.info(f"{subject}: Global radial strain rate calculated.")
+        except ValueError as e:
+            logger.warning(f"{subject}: {e} No global radial strain rate calculated.")
+
+        # * Feature 2  Peak-systolic strain, end-systolic strain, post-systolic strain
+
+        # We will use smoothed strain for all advanced features
+        fANCOVA = importr("fANCOVA")
+        x_r = FloatVector(time_grid_real)
+        y_r_gcs = FloatVector(circum_strain[16, :])
+        loess_fit_gcs = fANCOVA.loess_as(x_r, y_r_gcs, degree=2, criterion="gcv")
+        GCS_loess_x = np.array(loess_fit_gcs.rx2("x")).reshape(
+            T,
+        )
+        GCS_loess_y = np.array(loess_fit_gcs.rx2("fitted"))
+
+        y_r_grs = FloatVector(radial_strain[16, :])
+        loess_fit_grs = fANCOVA.loess_as(x_r, y_r_grs, degree=2, criterion="gcv")
+        GRS_loess_x = np.array(loess_fit_grs.rx2("x")).reshape(
+            T,
+        )
+        GRS_loess_y = np.array(loess_fit_grs.rx2("fitted"))
+
+        colors = ["blue"] * T
+        colors[T_ES] = "red"
+        colors[T_1_3_DD] = "green"
+        if T_pre_a is not None:
+            colors[T_pre_a] = "orange"
+
+        fig, ax1, ax2 = plot_time_series_double_x_y(
+            time_grid_point,
+            time_grid_real * 1000,
+            circum_strain[16, :],
+            GCS_loess_y,
+            "Time [frame]",
+            "Time [ms]",
+            "Strain [%]",
+            lambda x: x * temporal_resolution,
+            lambda x: x / temporal_resolution,
+            title=f"Subject {subject}: Global Circumferential Strain (GCS) Time Series",
+            colors=colors,
+        )
+        fig.savefig(f"{sub_dir}/timeseries/gcs.png")
+        plt.close(fig)
+
+        fig, ax1, ax2 = plot_time_series_double_x_y(
+            time_grid_point,
+            time_grid_real * 1000,
+            radial_strain[16, :],
+            GRS_loess_y,
+            "Time [frame]",
+            "Time [ms]",
+            "Strain [%]",
+            lambda x: x * temporal_resolution,
+            lambda x: x / temporal_resolution,
+            title=f"Subject {subject}: Global Radial Strain (GRS) Time Series",
+            colors=colors,
+        )
+        fig.savefig(f"{sub_dir}/timeseries/grs.png")
+        plt.close(fig)
+
+        # All following values are in absolute values!
+
+        circum_strain_ES = abs(GCS_loess_y[T_ES])
+
+        T_circum_strain_peak = np.argmax(abs(GCS_loess_y))
+        cicum_strain_peak = np.max(abs(GCS_loess_y))
+
+        T_circum_strain_post = None
+        circum_strain_post = None
+        if T_circum_strain_peak > T_ES:
+            # Post systolic shortening exists
+            T_circum_strain_post = T_circum_strain_peak
+            circum_strain_post = cicum_strain_peak
+
+            feature_dict.update(
+                {
+                    "LV: Circumferential Strain (End-systolic, absolute value) [%]": circum_strain_ES,
+                    "LV: Circumferential Strain (Post-systolic, absolute value) [%]": circum_strain_post,
+                }
+            )
+            logger.info(f"{subject}: End-systolic, post-systolic circumferential strain calculated.")
+        else:
+            feature_dict.update(
+                {
+                    "LV: Circumferential Strain (End-systolic, absolute value) [%]": circum_strain_ES,
+                    "LV:  Circumferential Strain (Peak-systolic, absolute value) [%]": cicum_strain_peak,
+                }
+            )
+            logger.info(f"{subject}: End-systolic, peak-systolic circumferential strain calculated.")
+
+        radial_strain_ES = abs(GRS_loess_y[T_ES])
+
+        T_radial_strain_peak = np.argmax(abs(GRS_loess_y))
+        radial_strain_peak = np.max(abs(GRS_loess_y))
+
+        T_radial_strain_post = None
+        radial_strain_post = None
+        if T_radial_strain_peak > T_ES:
+            # Post systolic shortening exists
+            T_radial_strain_post = T_radial_strain_peak
+            radial_strain_post = radial_strain_peak
+
+            feature_dict.update(
+                {
+                    "LV: Radial Strain (End-systolic, absolute value) [%]": radial_strain_ES,
+                    "LV: Radial Strain (Post-systolic, absolute value) [%]": radial_strain_post,
+                }
+            )
+            logger.info(f"{subject}: End-systolic, post-systolic radial strain calculated.")
+        else:
+            feature_dict.update(
+                {
+                    "LV: Radial Strain (End-systolic, absolute value) [%]": radial_strain_ES,
+                    "LV: Radial Strain (Peak-systolic, absolute value) [%]": radial_strain_peak,
+                }
+            )
+            logger.info(f"{subject}: End-systolic, peak-systolic radial strain calculated.")
+
+        # * Feature 3 Time to peak interval
+
+        t_circum_strain_peak = T_circum_strain_peak * temporal_resolution * 1000  # unit: ms
+        t_radial_strain_peak = T_radial_strain_peak * temporal_resolution * 1000
+
+        feature_dict.update(
+            {
+                "LV: Circumferential Strain: Time to Peak [ms]": t_circum_strain_peak,
+                "LV: Radial Strain: Time to Peak [ms]": t_radial_strain_peak,
+            }
+        )
+
+        ecg_processor = ECG_Processor(subject, args.retest)
+
+        if config.useECG and not ecg_processor.check_data_rest():
+            logger.warning(f"{subject}: No ECG rest data, time to peak strain index will not be calculated.")
+        else:
+            logger.info(f"{subject}: ECG rest data exists, calculate time to peak strain index.")
+            RR_interval = ecg_processor.determine_RR_interval()  # should be close to MeanNN in neurokit2
+
+            t_circum_strain_peak_index = t_circum_strain_peak / RR_interval
+            t_radial_strain_peak_index = t_radial_strain_peak / RR_interval
+
+            feature_dict.update(
+                {
+                    "LV: Circumferential Strain: Time to Peak Index": t_circum_strain_peak_index,
+                    "LV: Radial Strain: Time to Peak Index": t_radial_strain_peak_index,
+                }
+            )
+
+        # * Feature 4 Strain imaging diastolic index (SI-DI)
+
+        # Ref https://www.sciencedirect.com/science/article/pii/S0914508711000116
+        try:
+            circum_strain_1_3_DD = abs(GCS_loess_y[T_1_3_DD])
+
+            circum_SI_DI = (circum_strain_ES - circum_strain_1_3_DD) / circum_strain_ES
+
+            radial_strain_1_3_DD = abs(GRS_loess_y[T_1_3_DD])
+
+            radial_SI_DI = (radial_strain_ES - radial_strain_1_3_DD) / radial_strain_ES
+
+            feature_dict.update(
+                {
+                    "LV: Circumferential Strain: Strain Imaging Diastolic Index [%]": circum_SI_DI * 100,
+                    "LV: Radial Strain: Strain Imaging Diastolic Index [%]": radial_SI_DI * 100,
+                }
+            )
+        except ValueError:
+            logger.warning(f"{subject}: Error in calculating strain imaging diastolic index (SI-DI).")
+
+        # todo Feature 5 Torsion and recoil rate
+
+        df_row = pd.DataFrame([feature_dict])
+        df = pd.concat([df, df_row], ignore_index=True)
+
+    target_dir = config.features_visit2_dir if args.retest else config.features_visit1_dir
+    target_dir = os.path.join(target_dir, "strain")
+    os.makedirs(target_dir, exist_ok=True)
+    df.sort_index(axis=1, inplace=True)  # sort the columns according to alphabet orders
+    df.to_csv(os.path.join(target_dir, f"{args.file_name}.csv"), index=False)
