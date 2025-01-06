953cb52c86aced4d0dc80eb5e12a429970005720
YuukiAS <yuukias@unc.edu>
2025-01-06 02:40:15 -0500

    Fix Native T1 Segmentation
    For some cases, the segmentation of LV and myocardium should be swapped.

--- src/segmentation/Native_T1_20214/segment_native_t1.py
+++ src/segmentation/Native_T1_20214/segment_native_t1.py
@@ -2,6 +2,7 @@ import os
 import numpy as np
 import nibabel as nib
 import matplotlib.pyplot as plt
+import cv2
 import argparse
 import sys
 
@@ -54,5 +55,48 @@ if __name__ == "__main__":
 
     logger.info(f"{subject}: Retrieve segmented images")
     obtain_files_segmented(subject, 20214, "ShMOLLI", os.path.join(data_dir, f"seg_shmolli_t1map.nii.gz"))
+
+    # * For about 1/9 cases, the segmentation of LV and myocardium are swapped, we need to correct it.
+
+    nii_label_original = nib.load(os.path.join(data_dir, "seg_shmolli_t1map.nii.gz"))
+    label = nii_label_original.get_fdata()[:, :, 0, 0]
+
+    mask1 = label == 1
+    mask1 = mask1.astype(np.uint8)
+    mask2 = label == 2
+    mask2 = mask2.astype(np.uint8)
+
+    contours1, _ = cv2.findContours(mask1, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    contours2, _ = cv2.findContours(mask2, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    try:
+        contour1 = max(contours1, key=lambda x: cv2.arcLength(x, False))
+        contour2 = max(contours2, key=lambda x: cv2.arcLength(x, False))
+    except ValueError:
+        logger.error(f"{subject}: No contours found in the segmentation, the segmentation is invalid.")
+        os.remove(os.path.join(data_dir, f"seg_shmolli_t1map.nii.gz"))
+        sys.exit(1)
+
+
+    # >= 0 means the point is inside the contour
+    contour1_nest = all(cv2.pointPolygonTest(contour2, (int(round(point[0][0])), int(round(point[0][1]))), False) >= 0 for point in contour1)
+    contour2_nest = all(cv2.pointPolygonTest(contour1, (int(round(point[0][0])), int(round(point[0][1]))), False) >= 0 for point in contour2)
+
+    print(contour1_nest, contour2_nest)
+    # By default, contour2_nest should be True, we should correct it if contour1_nest is True
+    if contour1_nest:
+        logger.info(f"{subject}: Correcting the segmentation of LV and myocardium")
+        label_corrected = np.where(label == 1, 2, np.where(label == 2, 1, label))
+        nii_label_corrected = nib.Nifti1Image(label_corrected, nii_label_original.affine, nii_label_original.header)
+        os.remove(os.path.join(data_dir, f"seg_shmolli_t1map.nii.gz"))
+        nib.save(nii_label_corrected, os.path.join(data_dir, f"seg_shmolli_t1map.nii.gz"))
+    elif contour2_nest:
+        logger.info(f"{subject}: Segmentation of LV and myocardium is correct")
+    else:
+        # for visit2, there are around 284 such subjects
+        logger.error(f"{subject}: Segmentation of LV and myocardium is invalid")
+        os.remove(os.path.join(data_dir, f"seg_shmolli_t1map.nii.gz"))
+        sys.exit(1)
+
 
     logger.info(f"{subject}: Finish segmentation of native T1 mapping")
